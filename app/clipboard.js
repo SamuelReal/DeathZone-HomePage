e1b5fc3da1fb03aa64859c704b590f77820dd8d1
Samuel Real Rodríguez <samuelreal.tenerife@gmail.com>
2023-06-28 06:23:46 +0100

    modal feature

--- app/clipboard.js
+++ app/clipboard.js
@@ -1,12 +1,13 @@
 function copyServerIp() {
-    if (window.innerWidth < 1000) {
-        serverIp = "bedrock.deathzone.net";
-    } else {
-        serverIp = "play.deathzone.net";
-    }
-  
-    navigator.clipboard.writeText(serverIp)
-      .then(() => {
-        console.log("Texto copiado al portapapeles: " + serverIp);
-      });
+  if (window.innerWidth < 1000) {
+      serverIp = "bedrock.deathzone.net";
+  } else {
+      serverIp = "play.deathzone.net";
+  }
+
+  navigator.clipboard.writeText(serverIp)
+    .then(() => {
+      ipField = document.getElementById("showedIp");
+      ipField.textContent = serverIp;
+    });
   }
