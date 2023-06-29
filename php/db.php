2c5593e03caa1f32829a917d9d625f8c08e8e230
Samuel Real Rodríguez <samuelreal.tenerife@gmail.com>
2023-06-29 14:16:08 +0100

    clean back-end integration

--- php/db.php
+++ php/db.php
@@ -5,13 +5,9 @@ class db
     private $host = '51.83.47.197';
     private $user = 'deathzone.net';
     private $password = "YIuTm7OLq-b12q9.";
-    // private $host  = 'localhost';
-    // private $user  = 'root';
-    // private $password  = "";
     private $database = "deathzone.net";
     private $dbConnect = false;
-    public function __construct()
-    {
+    public function __construct() {
         if (!$this->dbConnect) {
             $conn = new mysqli($this->host, $this->user, $this->password, $this->database);
             if ($conn->connect_error) {
@@ -21,8 +17,7 @@ class db
             }
         }
     }
-    private function getData($sqlQuery)
-    {
+    private function getData($sqlQuery) {
         $result = mysqli_query($this->dbConnect, $sqlQuery);
         if (!$result) {
             die('Error in query: ' . mysqli_error($this->dbConnect) . " sqlQuery: " . $sqlQuery);
@@ -34,42 +29,7 @@ class db
         }
         return $data;
     }
-    private function getNumRows($sqlQuery)
-    {
-        $result = mysqli_query($this->dbConnect, $sqlQuery);
-        if (!$result) {
-            die('Error in query: ' . mysqli_error($this->dbConnect) . " sqlQuery: " . $sqlQuery);
-        }
-        $numRows = mysqli_num_rows($result);
-        return $numRows;
-    }
-    private function sqlQuery($sqlQuery)
-    { //ejecutar sql sin respuesta
-        $result = mysqli_query($this->dbConnect, $sqlQuery);
-        if (!$result) {
-            die('Error in query: ' . mysqli_error($this->dbConnect) . " sqlQuery: " . $sqlQuery);
-        }
-    }
-    private function orderArray($array)
-    { //ordenarlas para que empiezen desde el 0
-        $return = array();
-        foreach ($array as $a) {
-            array_push($return, $a);
-        }
-        return $return;
-    }
-    private function getRealIP()
-    {
-        if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-            return $_SERVER['HTTP_CLIENT_IP'];
-        } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-            return $_SERVER['HTTP_X_FORWARDED_FOR'];
-        } else {
-            return $_SERVER['REMOTE_ADDR'];
-        }
-    }
-    public function getStaffs()
-    {
+    public function getStaffs() {
         $sqlQuery = "SELECT * FROM `staffs` ORDER BY `order` ASC, `nickname` ASC";
         return $this->getData($sqlQuery);
     }
