73f1cd649c7663062c7e04786cf69f731dd702b4
Samuel Real Rodríguez <samuelreal.tenerife@gmail.com>
2023-06-30 02:46:44 +0100

    added loader icon

--- app/loader.js
+++ app/loader.js
@@ -1,7 +1,9 @@
 window.addEventListener('load', function() {
+    var loader = document.getElementById('loader');
     var content = document.getElementById('content');
     
     function showContent() {
+        loader.classList.add("invisible");
         content.classList.remove("invisible");
     }
     
